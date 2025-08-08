30b0077406565efda6f2045415da722e442d405a
Mikael Lönnbäck <mikkus@windowslive.com>
2025-08-08 09:28:42 +0300

    started feedback exercise

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,27 +1,14 @@
 import { useState } from 'react'
 
-const Display = props => <div>{props.value}</div>
-
-const Button = (props) => (
-  <button onClick={props.onClick}>
-    {props.text}
-  </button>
-)
-
 const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Display value={value} />
-      <Button onClick={() => setToValue(1000)} text='thousand' />
-      <Button onClick={() => setToValue(0)} text='reset' />
-      <Button onClick={() => setToValue(value + 1)} text='increment' />
+      code here
     </div>
   )
 }
