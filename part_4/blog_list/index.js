249b3414103ddfcc040f1e44431402cf569e951c
Mikael Lönnbäck <mikkus@windowslive.com>
2025-09-17 09:14:36 +0300

    Update

--- part_4/blog_list/index.js
+++ part_4/blog_list/index.js
@@ -7,6 +7,7 @@ const { tokenExtractor } = require('./middleware/tokenExtractor')
 const { userExtractor } = require('./middleware/userExtractor')
 const config =require('./utils/config')
 const loginRouter = require('./routes/login')
+const cors = require('cors')
 
 const app = express()
 
@@ -15,6 +16,7 @@ mongoose.connect(config.MONGODB_URI)
   .catch(err => console.error('Error connecting to MongoDB:', err.message))
 
 app.use(express.json())
+app.use(cors())
 app.use(tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
